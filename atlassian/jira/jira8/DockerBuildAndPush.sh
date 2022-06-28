13e81d086254b347b685a16482cc196d521fd823
GreenDamTan <GreenDamTan@users.noreply.github.com>
2022-06-28 20:13:25 +0800

    modify jira8容器构建脚本,改一下这个tag太长了

--- atlassian/jira/jira8/DockerBuildAndPush.sh
+++ atlassian/jira/jira8/DockerBuildAndPush.sh
@@ -1,2 +1,2 @@
-docker build -t makedie/jira-software:8.22.4-ubuntu-jdk-11.0.11_9-jdk-openj9-0.26.0 .
-docker push makedie/jira-software:8.22.4-ubuntu-jdk-11.0.11_9-jdk-openj9-0.26.0
+docker build -t makedie/jira-software:8.22.4-ubuntu-jdk-11.0.11_9-openj9-0.26.0 .
+docker push makedie/jira-software:8.22.4-ubuntu-jdk-11.0.11_9-openj9-0.26.0
