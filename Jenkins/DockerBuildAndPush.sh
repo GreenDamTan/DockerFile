aa9f38cb992eb4036ba12f41030a16597be59f6e
GreenDamTan <GreenDamTan@users.noreply.github.com>
2022-07-02 21:47:10 +0800

    modify 修改Jenkins的构建tag

--- Jenkins/DockerBuildAndPush.sh
+++ Jenkins/DockerBuildAndPush.sh
@@ -1,2 +1,2 @@
-docker build -t makedie/jenkins:2.346.1-lts-jdk-17.0.3+7_openj9-0.32.0 .
-docker push makedie/jenkins:2.346.1-lts-jdk-17.0.3+7_openj9-0.32.0
+docker build -t makedie/jenkins:2.346.1-lts-jdk-17.0.3_7-openj9-0.32.0 .
+docker push makedie/jenkins:2.346.1-lts-jdk-17.0.3_7-openj9-0.32.0
