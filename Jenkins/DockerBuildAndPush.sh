1a1eca5ef644b6c5ef6b28d2fdf80d469fe09555
GreenDamTan <GreenDamTan@users.noreply.github.com>
2022-07-02 21:43:59 +0800

    modify 修改Jenkins的jdk为ibm-semeru-runtimes:open-17.0.3_7-jdk-focal

--- Jenkins/DockerBuildAndPush.sh
+++ Jenkins/DockerBuildAndPush.sh
@@ -1,2 +1,2 @@
-docker build -t makedie/jenkins:2.346.1-lts-jdk-11.0.11_9-openj9-0.26.0 .
-docker push makedie/jenkins:2.346.1-lts-jdk-11.0.11_9-openj9-0.26.0
+docker build -t makedie/jenkins:2.346.1-lts-jdk-17.0.3+7_openj9-0.32.0 .
+docker push makedie/jenkins:2.346.1-lts-jdk-17.0.3+7_openj9-0.32.0
