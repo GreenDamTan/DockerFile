241931bfd3ba90a905ef56e31e077a37d63bab74
GreenDamTan <GreenDamTan@users.noreply.github.com>
2023-04-28 17:54:43 +0800

    [feat]proxmox 7.4-1

--- ProxmoxVE/DockerBuildAndPush.sh
+++ ProxmoxVE/DockerBuildAndPush.sh
@@ -1,5 +1,5 @@
 #docker build --squash -t makedie/proxmox_ve:7.2-1 .
-docker build -t makedie/proxmox_ve:7.3-3 .
-docker push makedie/proxmox_ve:7.3-3
-docker tag makedie/proxmox_ve:7.3-3 makedie/proxmox_ve:latest
+docker build -t makedie/proxmox_ve:7.4-1 .
+docker push makedie/proxmox_ve:7.4-1
+docker tag makedie/proxmox_ve:7.4-1 makedie/proxmox_ve:latest
 docker push makedie/proxmox_ve:latest
