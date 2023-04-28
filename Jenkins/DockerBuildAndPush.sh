f8312ed6c54f6b212abfa1fe4841ceced591cb09
GreenDamTan <GreenDamTan@users.noreply.github.com>
2023-04-28 17:41:07 +0800

    [feat]jenkins update jenkins:2.387.2-lts-jdk17

--- Jenkins/DockerBuildAndPush.sh
+++ Jenkins/DockerBuildAndPush.sh
@@ -1,4 +1,4 @@
-docker build -t makedie/jenkins:2.346.1-lts-jdk-17.0.3_7-openj9-0.32.0 .
-docker push makedie/jenkins:2.346.1-lts-jdk-17.0.3_7-openj9-0.32.0
-docker tag makedie/jenkins:2.346.1-lts-jdk-17.0.3_7-openj9-0.32.0 makedie/jenkins:latest
+docker build -t makedie/jenkins:2.387.2-lts-jdk17-openj9 .
+docker push makedie/jenkins:2.387.2-lts-jdk17-openj9
+docker tag makedie/jenkins:2.387.2-lts-jdk17-openj9 makedie/jenkins:latest
 docker push makedie/jenkins:latest
