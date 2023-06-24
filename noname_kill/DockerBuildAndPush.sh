4bd31c3f7c4662c6f7032b44ac1942ea71fab9e4
GreenDamTan <GreenDamTan@users.noreply.github.com>
2023-06-24 23:18:48 +0800

    [feat]noname_kill v1.9.121

--- noname_kill/DockerBuildAndPush.sh
+++ noname_kill/DockerBuildAndPush.sh
@@ -1,4 +1,4 @@
-docker build -t makedie/noname_kill:v1.9.114.3.1 .
-docker push makedie/noname_kill:v1.9.114.3.1
-docker tag makedie/noname_kill:v1.9.114.3.1 makedie/noname_kill:latest
+docker build -t makedie/noname_kill:v1.9.121 .
+docker push makedie/noname_kill:v1.9.121
+docker tag makedie/noname_kill:v1.9.121 makedie/noname_kill:latest
 docker push makedie/noname_kill:latest
