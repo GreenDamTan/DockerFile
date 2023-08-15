b5d5ee5e631125cd31caa5d9660f2705a5a0ad81
GreenDamTan <GreenDamTan@users.noreply.github.com>
2023-08-15 18:10:09 +0800

    [feat]proxmox 8.0.4

--- ProxmoxVE/DockerBuildAndPush.sh
+++ ProxmoxVE/DockerBuildAndPush.sh
@@ -1,5 +1,4 @@
-#docker build --squash -t makedie/proxmox_ve:8.0.3 .
-docker build -t makedie/proxmox_ve:8.0.3 .
-docker push makedie/proxmox_ve:8.0.3
-#docker tag makedie/proxmox_ve:8.0.3 makedie/proxmox_ve:latest
-#docker push makedie/proxmox_ve:latest
+name=makedie/proxmox_ve
+tag=8.0.4
+docker build -t $name:$tag .
+docker push $name:$tag
