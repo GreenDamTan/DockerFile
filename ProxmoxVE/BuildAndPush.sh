916316877c480655646bd369bf6233f9318f4a66
GreenDamTan <4781595+GreenDamTan@users.noreply.github.com>
2024-11-21 02:02:00 +0800

    [update]proxmox 8.2.9

--- ProxmoxVE/BuildAndPush.sh
+++ ProxmoxVE/BuildAndPush.sh
@@ -1,5 +1,5 @@
 name=makedie/proxmox_ve
-tag=8.2.8
+tag=8.2.9
 #DOCKER_BUILDKIT=1 docker build --network host -t $name:$tag .
 docker build -t $name:$tag .
-#docker push $name:$tag
+docker push $name:$tag
