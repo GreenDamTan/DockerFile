cfe82d816687bd3b34ccb8f59720970b371d68fe
GreenDamTan <4781595+GreenDamTan@users.noreply.github.com>
2024-12-13 00:57:01 +0800

    [update]fnOS TRIM-0.8.27-542.deb

--- fnOS/DockerBuildAndPush.sh
+++ fnOS/DockerBuildAndPush.sh
@@ -1,4 +1,6 @@
-docker build -t makedie/fnos:0.8.26-516_test .
-docker push makedie/fnos:0.8.26-516_test
-docker tag makedie/fnos:0.8.26-516_test makedie/fnos:latest
-docker push makedie/fnos:latest
+name=makedie/fnos
+tag=0.8.27-542_test
+docker build -t $name:$tag .
+docker push $name:$tag
+docker tag $name:$tag $name:latest
+docker push $name:latest
