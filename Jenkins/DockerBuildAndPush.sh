2080f8c5628334ff91d5a37531b20dee377ca334
GreenDamTan <4781595+GreenDamTan@users.noreply.github.com>
2025-06-11 10:59:37 +0800

    [update]jenkins update 2.504.2-lts-jdk17-openj9-17.0.15_6-jdk

--- Jenkins/DockerBuildAndPush.sh
+++ Jenkins/DockerBuildAndPush.sh
@@ -1,5 +1,5 @@
 name=makedie/jenkins
-tag=2.492.2-lts-jdk17-openj9-17.0.14_7-jdk
+tag=2.504.2-lts-jdk17-openj9-17.0.15_6-jdk
 docker build -t $name:$tag .
 docker push $name:$tag
 docker tag $name:$tag $name:latest
